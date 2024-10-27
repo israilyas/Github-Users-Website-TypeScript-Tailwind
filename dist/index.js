bf390e6293cff9aa4751de55cdb5098fb73b1c3c
ISRA ILYAS <israqureshi138@gmail.com>
2024-10-27 20:15:31 +0500

    project completed

--- dist/index.js
+++ dist/index.js
@@ -19,14 +19,14 @@ async function myCustomFetcher(url, option) {
 function showResultUI(singleUser) {
     const { avatar_url, location, login, id, url } = singleUser;
     main_container.insertAdjacentHTML("beforeend", `
-            <div class="bg-[#15253C]   flex-col align-center justify-center shadow-xl shadow-red rounded hover:scale-[1.05] transition-all duration-500 p-[5px] w-[170px] shadow-sm hover:shadow-[#6D28D980] ">
+            <div class="bg-[#15253C]   flex-col items-center justify-center rounded hover:-translate-y-1 transition-all duration-500 p-[5px] md:w-[220px] w-[170px] xsm:w-[220px] hover:shadow-lg  hover:shadow-[#6D28D980] ">
                 <img src=${avatar_url} alt="${login}" class="w-[100%]"/>
 
                 <hr class="w-[100%] h-[2px] bg-white align-center m-auto my-3" />
 
-                <div class="card-footer flex flex-row items-center justify-center gap-[15px]">
-                    <img src=${avatar_url} class="w-[30%] rounded-full p-0 m-0"/>
-                    <a href="${url}" class="text-bold uppercase "> Github</a>
+                <div class="card-footer flex flex-row items-center justify-around pb-2">
+                    <img src=${avatar_url} class="w-[20%] rounded-full p-0 m-0"/>
+                    <a href=${url} class="text-bold uppercase "> ${login}</a>
                 </div>
 
             </div>
@@ -49,17 +49,17 @@ fetchUserData("https://api.github.com/users");
 // Let's Perform Search Functionality
 formSubmit.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const searchTerm = getUserName.value.toLocaleLowerCase();
+    const searchTerm = getUserName.value.toLowerCase();
     try {
-        const url = "https://api.github.com/users";
+        const url = "https://api.github.com/search/users?q=${searchTerm}";
         const allUserData = await myCustomFetcher(url, {});
         const matchingUsers = allUserData.filter((user) => {
-            return user.login.toLocaleLowerCase().includes(searchTerm);
+            return user.login.toLowerCase().includes(searchTerm);
         });
         // Clear Previous Data
-        main_container.innerHTML = '';
+        main_container.innerHTML = "";
         if (matchingUsers.length === 0) {
-            main_container?.insertAdjacentHTML("beforeend", `<p class="empty-msg">No matching user</p>`);
+            main_container.insertAdjacentHTML("beforeend", `<p class="empty-msg">No matching user</p>`);
         }
         else {
             for (const singleUser of matchingUsers) {
